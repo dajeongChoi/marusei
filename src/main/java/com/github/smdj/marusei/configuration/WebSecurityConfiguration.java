77d941db1e5d066648a115df7b395b38434efc9a
dajeongChoi <dajeong9393@gmail.com>
2019-01-26 16:56:10 +0900

    Fix 로그인 후 top page 403에러 처리(WebSecurityConfiguration 엑세스 권한 부여)

--- src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.github.smdj.marusei.configuration;
 
+import com.github.smdj.marusei.security.AccountDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,14 +15,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AccountDetailsService accountDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(accountDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
-        http.authorizeRequests().antMatchers("/", "/signup").anonymous();
+        http.formLogin().permitAll();
+        http.authorizeRequests().antMatchers("/signup").anonymous()
+                .antMatchers("/").permitAll();
     }
 }
